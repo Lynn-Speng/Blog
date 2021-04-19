b2bf3eec340d7ed7d0505a52b1099ede80e10628
Lynn Speng <Lynn.Speng@outlook.com>
2021-04-20 00:35:08 +0800

    fix: add proposal code support by babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
+  presets: [
+    [
+      "@vue/cli-plugin-babel/preset",
+      {
+        useBuiltIns: "usage",
+        corejs: {
+          version: "3.10",
+          proposals: true,
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        corejs: {
+          version: 3,
+          proposals: true,
+        },
+      },
+    ],
+  ],
 };
